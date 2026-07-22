511e745923144241904d58f4b6970e1597421052
Mridul Pandey <mridulpandey8355@gmail.com>
2026-07-22 23:50:14 +0530

    Updated ALU module

--- RISCV_SingleCycle.srcs/sources_1/new/ALU.v
+++ RISCV_SingleCycle.srcs/sources_1/new/ALU.v
@@ -21,6 +21,68 @@
 
 
 module ALU(
+    input  signed [31:0] A,
+    input  signed [31:0] B,
+    input        [2:0]  ALUControl,
 
-    );
+    output reg signed [31:0] Result,
+    output                  Zero,
+    output reg              Carry,
+    output reg              Overflow,
+    output                  Negative
+);
+
+reg [32:0] temp;
+
+always @(*) begin
+
+    Carry     = 1'b0;
+    Overflow  = 1'b0;
+
+    case(ALUControl)
+
+        // ADD
+        3'b000: begin
+            temp   = {1'b0,A} + {1'b0,B};
+            Result = temp[31:0];
+            Carry  = temp[32];
+
+            Overflow =
+                (~A[31] & ~B[31] & Result[31]) |
+                ( A[31] &  B[31] & ~Result[31]);
+        end
+
+        // SUB
+        3'b001: begin
+            temp   = {1'b0,A} - {1'b0,B};
+            Result = temp[31:0];
+            Carry  = temp[32];
+
+            Overflow =
+                (~A[31] &  B[31] & Result[31]) |
+                ( A[31] & ~B[31] & ~Result[31]);
+        end
+
+        // AND
+        3'b010:
+            Result = A & B;
+
+        // OR
+        3'b011:
+            Result = A | B;
+
+        // SLT
+        3'b101:
+            Result = (A < B) ? 32'd1 : 32'd0;
+
+        default:
+            Result = 32'd0;
+
+    endcase
+
+end
+
+assign Zero     = (Result == 32'd0);
+assign Negative = Result[31];
+
 endmodule
